0401a3fecd288160b8988f822b84f7d14b97de66
rahmad2000 <139571072+rahmad2000@users.noreply.github.com>
2024-05-13 15:30:38 -0500

    S2E26

--- starting-project/src/components/NewPost.jsx
+++ starting-project/src/components/NewPost.jsx
@@ -1,15 +1,19 @@
+import { useState } from 'react';
 import classes from './NewPost.module.css';
 
 function NewPost() {
+    const [enterBody, setEnterBody] = useState('');
+
     function changeBodyHandler(event) {
-    console.log(event.target.value);
+        setEnterBody(event.target.value);
     }
     return (
         <form className={classes.form}>
             <p>
                 <label htmlFor="body">Text</label>
-                <textarea id="body" required rows={3} onChange={changeBodyHandler}/>
+                <textarea id="body" required rows={3} onChange={changeBodyHandler} />
             </p>
+            <p>{enterBody}</p>
             <p>
                 <label htmlFor="name">Your name</label>
                 <input type="text" id="name" required />
