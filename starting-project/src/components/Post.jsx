59edeb42fb879b236c1ed42697a078889f4f3887
rahmad2000 <139571072+rahmad2000@users.noreply.github.com>
2024-05-13 11:25:46 -0500

    Chosen Name S2E18

--- starting-project/src/components/Post.jsx
+++ starting-project/src/components/Post.jsx
@@ -1,7 +1,10 @@
+const names = ['Tyrion', 'Jon'];
+
 function Post() {
+    const chosenName = Math.random() > 0.5 ? names[0] : names [1];
     return (
         <div>
-            <p>Tyrion</p>
+            <p>{chosenName}</p>
             <p>That is a Dragon!</p>
         </div>
     );
