19e7b0f216904227df57503d1d53493f3b77c56a
rahmad2000 <139571072+rahmad2000@users.noreply.github.com>
2024-05-13 11:41:53 -0500

    S2E20

--- starting-project/src/App.jsx
+++ starting-project/src/App.jsx
@@ -1,8 +1,8 @@
 import Post from "./components/Post";
 function App() {
   return <main>
-    <Post></Post>
-    <Post></Post>
+    <Post author='Tyrion' body='I am kicked out of my Family.'></Post>
+    <Post author='Jon' body='Family what a strange concept.'></Post>
     <Post></Post>
   </main>;
 }
