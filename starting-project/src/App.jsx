c547b409559cba53ba7d6cfa39cd4717e9fc91a4
rahmad2000 <139571072+rahmad2000@users.noreply.github.com>
2024-05-13 11:55:25 -0500

    S2E21

--- starting-project/src/App.jsx
+++ starting-project/src/App.jsx
@@ -3,7 +3,6 @@ function App() {
   return <main>
     <Post author='Tyrion' body='I am kicked out of my Family.'></Post>
     <Post author='Jon' body='Family what a strange concept.'></Post>
-    <Post></Post>
   </main>;
 }
 
