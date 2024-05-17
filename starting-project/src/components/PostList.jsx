dae5ae27c67ecddda64625aef4b3b3308288b375
rahmad2000 <139571072+rahmad2000@users.noreply.github.com>
2024-05-17 09:08:44 -0500

    S2E26

--- starting-project/src/components/PostList.jsx
+++ starting-project/src/components/PostList.jsx
@@ -1,14 +1,27 @@
+import {useState} from 'react';
 import Post from './Post';
 import NewPost from './NewPost';
 import classes from './PostList.module.css'
 
+
 function PostList() {
+    const [enterBody, setEnterBody] = useState('');
+    const [enterAuthor, setEnterAuthor] = useState('');
+
+    function bodyChangeHandler(event) {
+        setEnterBody(event.target.value);
+    }
+
+    function authorChangeHandler(event) {
+        setEnterAuthor(event.target.value);
+    }
+    
     return (
         <>
-            <NewPost></NewPost>
+            <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler}></NewPost>
             <ul className={classes.posts}>
-                <Post author='Tyrion' body='I am kicked out of my Family.'></Post>
-                <Post author='Jon' body='Family what a strange concept.'></Post>
+                <Post author={enterAuthor} body={enterBody}></Post>
+                <Post author='Jon' body='I Have a Family!'></Post>
             </ul>
         </>
     );
