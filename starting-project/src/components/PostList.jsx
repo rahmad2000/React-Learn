c3b681b92ea74c689168f221ff5dfc628aa0e386
rahmad2000 <139571072+rahmad2000@users.noreply.github.com>
2024-05-17 10:44:56 -0500

    S2E28

--- starting-project/src/components/PostList.jsx
+++ starting-project/src/components/PostList.jsx
@@ -7,8 +7,13 @@ import classes from './PostList.module.css'
 
 
 function PostList() {
+    const [modalIsVisible, setModalIsVisible] = useState(true)
     const [enterBody, setEnterBody] = useState('');
     const [enterAuthor, setEnterAuthor] = useState('');
+
+    function hideModalHandler() {
+        setModalIsVisible(false);
+    }
 
     function bodyChangeHandler(event) {
         setEnterBody(event.target.value);
@@ -20,12 +25,14 @@ function PostList() {
 
     return (
         <>
-            <Modal>
+            {modalIsVisible ? (
+            <Modal onClose = {hideModalHandler}>
                 <NewPost
                     onBodyChange={bodyChangeHandler}
                     onAuthorChange={authorChangeHandler}>
                 </NewPost>
             </Modal>
+            ) : false}
             <ul className={classes.posts}>
                 <Post author={enterAuthor} body={enterBody}></Post>
                 <Post author='Jon' body='I Have a Family!'></Post>
