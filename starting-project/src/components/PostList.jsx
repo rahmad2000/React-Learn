dba4a7f69c86399ab801b837058f6ba504840a5b
rahmad2000 <139571072+rahmad2000@users.noreply.github.com>
2024-05-27 11:49:09 -0500

    S2 E36

--- starting-project/src/components/PostList.jsx
+++ starting-project/src/components/PostList.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Post from './Post';
 import NewPost from './NewPost';
 import Modal from './Modal';
@@ -7,43 +7,53 @@ import { CgEnter } from 'react-icons/cg';
 
 
 
-function PostList({postIsVisible, onStopPosting,}) {
-    const [post, setPost] = useState([]);
+function PostList({ postIsVisible, onStopPosting, }) {
+    const [posts, setPosts] = useState([]);
 
-    function addPostHandler(postData) {
-        fetch('http://localhost:8080/posts', {
-            method: 'POST',
-            body: JSON.stringify(postData),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-        setPost( (existingPosts) => [postData, ...existingPosts]);
-    }
-    return (
-        <>
-            {postIsVisible ? (
-            <Modal onClose = {onStopPosting}>
+    useEffect(() => {
+        async function fetchPosts() {
+            const response = await fetch('http://localhost:8080/posts')
+            const resData = await response.json();
+            setPosts(resData.posts);
+        }
+
+        fetchPosts();
+    }, []);
+
+function addPostHandler(postData) {
+    fetch('http://localhost:8080/posts', {
+        method: 'POST',
+        body: JSON.stringify(postData),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    setPost((existingPosts) => [postData, ...existingPosts]);
+}
+return (
+    <>
+        {postIsVisible ? (
+            <Modal onClose={onStopPosting}>
                 <NewPost
-                    onCancel = {onStopPosting}
-                    onAddPost = {addPostHandler}
+                    onCancel={onStopPosting}
+                    onAddPost={addPostHandler}
                 >
                 </NewPost>
             </Modal>
-            ) : false}
-            {post.length > 0 && (
+        ) : false}
+        {posts.length > 0 && (
             <ul className={classes.posts}>
-                {post.map((post) => <Post key={post.body} author={post.author} body={post.body}></Post>)}
+                {posts.map((post) => <Post key={post.body} author={post.author} body={post.body}></Post>)}
             </ul>
-            )}
-            {post.length === 0 && (
-                <div style={{textAlign: 'center', color: 'white'}}>
-                    <h2>There are no Post yet.</h2>
-                    <p>Start adding some!</p>
-                </div>
-            )}
-        </>
-    );
+        )}
+        {posts.length === 0 && (
+            <div style={{ textAlign: 'center', color: 'white' }}>
+                <h2>There are no Post yet.</h2>
+                <p>Start adding some!</p>
+            </div>
+        )}
+    </>
+);
 }
 
 export default PostList;
