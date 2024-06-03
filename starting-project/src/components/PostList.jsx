bb5bb581e005c88981983cab7d9ce1fa4fc1aeec
rahmad2000 <139571072+rahmad2000@users.noreply.github.com>
2024-06-03 08:46:54 -0500

    S2 E38

--- starting-project/src/components/PostList.jsx
+++ starting-project/src/components/PostList.jsx
@@ -9,12 +9,15 @@ import { CgEnter } from 'react-icons/cg';
 
 function PostList({ postIsVisible, onStopPosting, }) {
     const [posts, setPosts] = useState([]);
+    const [isFetching, setIsFetching] = useState(false);
 
     useEffect(() => {
         async function fetchPosts() {
+            setIsFetching(true);
             const response = await fetch('http://localhost:8080/posts')
             const resData = await response.json();
             setPosts(resData.posts);
+            setIsFetching(false);
         }
 
         fetchPosts();
@@ -41,15 +44,20 @@ return (
                 </NewPost>
             </Modal>
         ) : false}
-        {posts.length > 0 && (
+        {!isFetching && posts.length > 0 && (
             <ul className={classes.posts}>
                 {posts.map((post) => <Post key={post.body} author={post.author} body={post.body}></Post>)}
             </ul>
         )}
-        {posts.length === 0 && (
+        {!isFetching && posts.length === 0 && (
             <div style={{ textAlign: 'center', color: 'white' }}>
                 <h2>There are no Post yet.</h2>
                 <p>Start adding some!</p>
+            </div>
+        )}
+        {isFetching && (
+            <div style={{textAlign: 'center', color: 'white'}}>
+                <p>Loading Posts...</p>
             </div>
         )}
     </>
